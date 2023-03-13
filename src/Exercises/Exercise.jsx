c6ae68bc467cc2d7b536954d237cd00f7c83dc4e
Saleh Kalouti <salehkalouti@gmail.com>
2023-03-13 05:15:54 +0200

    final styling

--- src/Exercises/Exercise.jsx
+++ src/Exercises/Exercise.jsx
@@ -1,8 +1,12 @@
 import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, Link } from "react-router-dom";
 import classes from "./Exercise.module.css";
 import { green } from "@mui/material/colors";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import { Typography } from "@mui/material";
+import { getData } from "../database/getData.mjs";
 
 export default function Exercise() {
   const params = useParams();
@@ -22,25 +26,90 @@ export default function Exercise() {
     }
   };
 
+  const breadcrumbs = [
+    <Link
+      to="/dashBoard"
+      underline="underline"
+      key="2"
+      style={{ textDecoration: "none", color: "#0D2F4E" }}
+    >
+      DashBoard
+    </Link>,
+    <Link
+      to="/dashBoard/myCoursesPage"
+      underline="underline"
+      key="2"
+      style={{ textDecoration: "none", color: "#0D2F4E" }}
+    >
+      My Courses
+    </Link>,
+    <Link
+      to={`/courses/${params.courseId}`}
+      underline="underline"
+      key="2"
+      style={{ textDecoration: "none", color: "#0D2F4E" }}
+    >
+      course {params.courseId}
+      {/* {isData ? (error1 ? course.course_title : "course") : "course"} */}
+    </Link>,
+    <Link
+      to={`/courses/${params.courseId}`}
+      underline="underline"
+      key="2"
+      style={{ textDecoration: "none", color: "#0D2F4E" }}
+    >
+      Lesson {params.lessonId}
+      {/* {isData ? (error1 ? course.course_title : "course") : "course"} */}
+    </Link>,
+    <Typography key="3" color="text.primary">
+      Exercise {state.id}
+    </Typography>,
+  ];
+
   return (
     <div className={classes.exercise}>
+      {console.log(state)}
+      <div
+        style={{
+          display: "flex",
+          alignSelf: "flex-start",
+          marginTop: "15px",
+        }}
+      >
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+          fontSize="large"
+        >
+          {breadcrumbs}
+        </Breadcrumbs>
+      </div>
       <div
         className={classes.exercise_container}
         style={{ backgroundColor: color }}
       >
         <h1>{question}</h1>
-        {answer_choices.map((answer) => {
-          return (
-            <Button
-              variant="contained"
-              onClick={() => {
-                answering(answer);
-              }}
-            >
-              {answer}
-            </Button>
-          );
-        })}
+        <div
+          style={{
+            display: "flex",
+            width: "70%",
+            justifyContent: "space-evenly",
+            marginTop: "50px",
+          }}
+        >
+          {answer_choices.map((answer) => {
+            return (
+              <Button
+                variant="contained"
+                onClick={() => {
+                  answering(answer);
+                }}
+              >
+                {answer}
+              </Button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/pages/LessonPage/LessonPage.jsx
+++ src/pages/LessonPage/LessonPage.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./LessonPage.module.css";
 import { useLocation, useNavigate, useParams, Link } from "react-router-dom";
 import { BsFillArrowRightSquareFill } from "react-icons/bs";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import { Typography } from "@mui/material";
+import { getData } from "../../database/getData.mjs";
 
 export default function LessonPage() {
   const params = useParams();
   console.log("params", params);
+  const [course, setCourse] = useState();
+  const [isData, setIsData] = useState();
+  const [error1, setError1] = useState();
+  const [courseTitle, setCourseTitle] = useState("course");
 
   const { state } = useLocation();
   const { dialogue, vocabulary, practice_questions } = state.lesson_content;
   const navigate = useNavigate();
+
+  const { isLogged, user, userId } = JSON.parse(
+    localStorage.getItem("loginInfo")
+  );
+
+  useEffect(() => {
+    const data = getData(
+      setError1,
+      error1,
+      `https://6405b55440597b65de3e8d49.mockapi.io/language/courses/${params.courseId}`
+    );
+    data.then((result) => {
+      if (!error1) {
+        setCourse(result);
+        setIsData(true);
+        setCourseTitle(course.course_title);
+      }
+    });
+  }, []);
 
   const breadcrumbs = [
     <Link
@@ -37,7 +61,8 @@ export default function LessonPage() {
       key="2"
       style={{ textDecoration: "none", color: "#0D2F4E" }}
     >
-      course.course_title
+      {courseTitle}
+      {/* {isData ? (error1 ? course.course_title : "course") : "course"} */}
     </Link>,
     <Typography key="3" color="text.primary">
       {state.lesson_title}
@@ -45,64 +70,80 @@ export default function LessonPage() {
   ];
 
   return (
-    <div className={classes.lessonPage}>
-      <div className={classes.lessonPage_container}>
-        <div
-          style={{
-            display: "flex",
-            alignSelf: "flex-start",
-            marginTop: "15px",
-          }}
-        >
-          <Breadcrumbs
-            separator={<NavigateNextIcon fontSize="small" />}
-            aria-label="breadcrumb"
-            fontSize="large"
-          >
-            {breadcrumbs}
-          </Breadcrumbs>
-        </div>
-        <div className={classes.lessonPage_dialogue}>
-          {dialogue.map((el) => {
-            return (
-              <div>
-                <p>{el.speaker}</p>
-                <p>{el.line}</p>
+    <>
+      {console.log("inlesson page", course)}
+      {isData ? (
+        error1 ? (
+          <div>error</div>
+        ) : (
+          <div className={classes.lessonPage}>
+            <div className={classes.lessonPage_container}>
+              <div
+                style={{
+                  display: "flex",
+                  alignSelf: "flex-start",
+                  marginTop: "15px",
+                }}
+              >
+                <Breadcrumbs
+                  separator={<NavigateNextIcon fontSize="small" />}
+                  aria-label="breadcrumb"
+                  fontSize="large"
+                >
+                  {breadcrumbs}
+                </Breadcrumbs>
               </div>
-            );
-          })}
-        </div>
-        <hr />
-        <div className={classes.lessonPage_vocabulary}>
-          {vocabulary.map((el) => {
-            return (
-              <div>
-                <p>{el.english_word}</p>
-                <p>{el.target_word}</p>
+              <div className={classes.lessonPage_dialogue}>
+                {dialogue.map((el) => {
+                  return (
+                    <div style={{}}>
+                      <p>{el.speaker}</p>
+                      <p>{el.line}</p>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-        <hr />
-        <div className={classes.lessonPage_practice}>
-          {practice_questions.map((exercise) => {
-            return (
-              <div>
-                <h1>exercise number {exercise.id}</h1>
-                <BsFillArrowRightSquareFill
-                  size="1.5rem"
-                  onClick={() => {
-                    navigate(
-                      exercise.id + `?x=${encodeURI("exercise" + exercise.id)}`,
-                      { state: exercise }
-                    );
-                  }}
-                />
+              <hr width="500px" />
+              <div className={classes.lessonPage_vocabulary}>
+                {vocabulary.map((el) => {
+                  return (
+                    <div>
+                      <p>{el.english_word}</p>
+                      <p>{el.target_word}</p>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
+              <hr height="500px" />
+              <div className={classes.lessonPage_practice}>
+                {practice_questions.map((exercise) => {
+                  return (
+                    <div>
+                      <h1>exercise number {exercise.id}</h1>
+                      <BsFillArrowRightSquareFill
+                        size="1.5rem"
+                        onClick={() => {
+                          navigate(
+                            exercise.id +
+                              `?x=${encodeURI("exercise" + exercise.id)}`,
+                            { state: exercise }
+                          );
+                        }}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )
+      ) : (
+        <div className={classes.home}>
+          <div className={classes.home_container}>
+            <div className={classes.home_loader}></div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
